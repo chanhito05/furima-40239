02feb64c3592e815ff6b225630f6d4d2047fcd5a
chanhito05 <koumeto0511@gmail.com>
2024-05-21 20:36:18 +0900

    エラーハンドリングの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
   validates :birthday, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合で入力してください' }
 end
