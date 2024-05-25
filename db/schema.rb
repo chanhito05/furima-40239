a87911129adc93688ae64ec75f25a1801e7a739e
chanhito05 <koumeto0511@gmail.com>
2024-05-25 18:57:21 +0900

    テストコード作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_20_051804) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_25_002954) do
   create_table "article", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "text", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_20_051804) do
     t.string "last_name_kana"
     t.string "first_name_kana"
     t.date "birthday"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
