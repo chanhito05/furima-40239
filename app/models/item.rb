78330439253c4df8c574ebde89ff496ed1206941
chanhito05 <koumeto0511@gmail.com>
2024-06-01 21:10:49 +0900

    ダミー画像が登録される

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
   belongs_to :shipping_time
 
   has_one_attached :image
+  has_one :order
 
   validates :image, presence: true
   validates :name, presence: true
@@ -18,4 +19,10 @@ class Item < ApplicationRecord
   validates :shipping_area_id, presence: true, numericality: { other_than: 1 }
   validates :shipping_time_id, presence: true, numericality: { other_than: 1 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+  def sold_out?
+    order.present?
+  end
 end
+
+
