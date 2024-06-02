507a33c5f35f2136ae2e7b710bc9df26ad770ea7
chanhito05 <koumeto0511@gmail.com>
2024-06-02 21:32:31 +0900

    詳細ページの設定と条件分岐の実装

--- db/migrate/20240602081847_create_purchases.rb
+++ db/migrate/20240602081847_create_purchases.rb
@@ -1,11 +1,13 @@
 class CreatePurchases < ActiveRecord::Migration[7.0]
   def change
-    create_table :purchases do |t|
 =begin
+    create_table :purchases do |t|
+
       t.references :item, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-=end
+
       t.timestamps
     end
+=end
   end
 end
