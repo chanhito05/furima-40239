b7f349eb998f71237791b6804a6e117bb53f6b76
chanhito05 <koumeto0511@gmail.com>
2024-06-02 18:41:59 +0900

    商品詳細ページに遷移できる

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,29 @@
+class PurchasesController < ApplicationController
+  before_action :set_item
+
+  def new
+    @purchase = Purchase.new
+  end
+
+  def create
+    @purchase = Purchase.new(purchase_params)
+    @purchase.item = @item
+    @purchase.user = current_user
+
+    if @purchase.save
+      redirect_to @item, notice: '購入が完了しました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def purchase_params
+    params.require(:purchase).permit(:credit_card_number, :expiration_date, :cvv)
+  end
+end
