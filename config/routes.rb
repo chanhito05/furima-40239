d51de38587e610ddd04316ab5cd7cd476ff265aa
chanhito05 <koumeto0511@gmail.com>
2024-06-03 14:23:39 +0900

    編集ボタンの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items do
-  # resources :purchases, only: [:new, :create]
+   resources :purchases, only: [:new, :create]
   end
   root to: 'items#index'
 end
